fffaafe0610a86b190c89660ab92a57453c2cd78
Ty74 <31639730+Ty74@users.noreply.github.com>
2017-09-10 14:58:17 -0700

    diction in tuples out
    dictionary in tuples out

---
+++ AssignmentDictionarytuplesout.py
@@ -0,0 +1,13 @@
+def dictionaryInTuplesOut():
+    mydict={
+        "Speros":"(555)-555-5555",
+        "Michael":"(999)999-9999",
+        "Jay":"(777)777-7777"
+
+    }
+
+    for i in mydict.iteritems():
+        list=[i]
+        print list
+
+dictionaryInTuplesOut()
